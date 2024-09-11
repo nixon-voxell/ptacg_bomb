bee0b53f5c61d6bec64e933bd90b4b71a530d194
Diviyan20 <diviyanr@gmail.com>
2024-09-11 15:28:03 +0800

    feat: Spawn 2D Camera

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@
 #![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
 
 use bevy::prelude::*;
+mod spawn_camera;
+use spawn_camera::CameraPlugin;
 
 // TODO: fix release workflow namings
 fn main() -> AppExit {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_plugins(CameraPlugin)
+    .run()
 }
